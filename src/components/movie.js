fa38e33ec1a89f2128511f3c0c2a16d52add637e
rslibed <rslibed@gmail.com>
2018-01-19 15:29:47 -0800

    Set up routing pages with react router dom

---
+++ src/components/movie.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieQuotes = props => {
+    return (
+        <div>
+            <h1 className="center-align">Movie Quotes</h1>
+            <p>I'll be back!</p>
+        </div>
+    )
+}
+
+export default MovieQuotes;
